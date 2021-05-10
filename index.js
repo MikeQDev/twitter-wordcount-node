42c9b7c0fd215cd1c594121da3b6bebf29c5e911
mike <m@d>
2021-05-10 10:26:31 -0400

    print topN or all words

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const twitterAccessor = require('./twitter-accessor');
 const wordCounter = require('./word-counter');
 
-const topNWords = 4; // # of most-common words to print
+// # of most-common words to print. Set >0 to execute, <0 will print all
+const topNWords = -1;
+const tweetQuery = 'i wish there was an app';
 
 twitterAccessor.getAggregatedTweets(
-  'i wish there was an app that',
+  tweetQuery,
 ).then((allTweets) => {
-  console.log(wordCounter(allTweets).slice(0, topNWords));
+  console.log('Working with', allTweets.length, 'tweets for query `', tweetQuery, '`');
+  if (topNWords > 0) {
+    // for printing just topN common words
+    console.log(wordCounter(allTweets).slice(0, topNWords));
+  } else {
+    // print all words that occurs more than once
+    wordCounter(allTweets).forEach((wordCount) => {
+      if (wordCount.count > 2) {
+        console.log(wordCount);
+      }
+    });
+  }
 });
