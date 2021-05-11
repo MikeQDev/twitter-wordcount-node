760561d01d56701686114690e21b27cea0567508
mike <m@d>
2021-05-11 17:47:34 -0400

    perform word-count on phrases, write output to file

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
+const fs = require('fs');
 const twitterAccessor = require('./twitter-accessor');
 const wordCounter = require('./word-counter');
 
 // # of most-common words to print. Set >0 to execute, <0 will print all
 const topNWords = -1;
+const wordSequenceCount = 5; // # of words to group together in count
 const tweetQuery = 'i wish there was an app';
+const tweetDataOutPath = `${tweetQuery.replace(/[^A-Za-z]/g, '')}-${Date.now()}-output.json`;
+
+if (wordSequenceCount < 1) {
+  console.log('Word sequence count must be >= 1');
+  process.exit(1);
+}
 
 twitterAccessor.getAggregatedTweets(
   tweetQuery,
 ).then((allTweets) => {
   console.log('Working with', allTweets.length, 'tweets for query `', tweetQuery, '`');
+  // write tweets to file for follow-up
+  fs.writeFile(tweetDataOutPath, JSON.stringify(allTweets), (err) => {
+    if (err) {
+      console.log('Error writing file', err);
+    } else {
+      console.log('Successfully wrote file');
+    }
+  });
+
+  // NOTE: watch out duplicate words/phrases from retweets, 'RT @user ...'
   if (topNWords > 0) {
     // for printing just topN common words
     console.log(wordCounter(allTweets).slice(0, topNWords));
   } else {
     // print all words that occurs more than once
-    wordCounter(allTweets).forEach((wordCount) => {
+    wordCounter(allTweets, wordSequenceCount).forEach((wordCount) => {
       if (wordCount.count > 2) {
         console.log(wordCount);
       }
--- word-counter.js
+++ word-counter.js
@@ -1,14 +1,25 @@
-// TODO: attempt 2-word count? IE: 'the fox' as one entry instead of two
-module.exports = (tweetData) => {
+// tweetData = array of tweets
+// wordSequenceCount = # of words to group together for counting
+module.exports = (tweetData, wordSequenceCount) => {
   // Perform word-count
   const wordCounts = {};
   tweetData.forEach((tweet) => {
-    tweet.text.split(/[ ,.?]+/).forEach((word) => { wordCounts[word] = word in wordCounts ? wordCounts[word] + 1 : 1; });
+    const split = tweet.text.split(/[ ,.?]+/);
+    split.forEach((word, index) => {
+      // one-word count
+      // wordCounts[word] = word in wordCounts ? wordCounts[word] + 1 : 1;
+      const wordPair = index + wordSequenceCount <= split.length
+        ? split.slice(index, index + wordSequenceCount).join(' ')
+        : ''; // This probably isn't efficient
+      wordCounts[wordPair] = wordPair in wordCounts ? wordCounts[wordPair] + 1 : 1;
+    });
   });
 
   // Sort
   const sortable = [];
-  Object.keys(wordCounts).forEach((word) => { sortable.push({ word, count: wordCounts[word] }); });
+  Object.keys(wordCounts).forEach((word) => {
+    sortable.push({ word, count: wordCounts[word] });
+  });
   sortable.sort((a, b) => b.count - a.count);
   return sortable;
 };
